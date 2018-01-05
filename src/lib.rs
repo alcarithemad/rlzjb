57ebc5d731c3f240f4bf51417c210fae2d12a238
Colin Valliant <alcarithemad@gmail.com>
2018-01-04 19:57:50 -0800

    Add better error handling.

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,20 @@ use std::mem;
 use std::slice;
 use std::vec;
 
-pub fn decompress(bs: &[u8], size: usize) -> Vec<u8> {
+pub fn decompress(bs: &[u8], size: usize) -> Option<Vec<u8>> {
     let blen = bs.len();
     let mut out: Vec<u8> = Vec::with_capacity(blen);
     let mut pos = 0;
     while pos+9 < blen && out.len() < size {
-        let control = bs[pos];
+        let control = bs.get(pos)?;
         pos += 1;
         for i in 0..8 {
             if control & (1 << i) == 0 {
-                out.push(bs[pos]);
+                out.push(*bs.get(pos)?);
                 pos += 1;
             } else {
-                let length = ((bs[pos] >> 2) + 3) as usize;
-                let distance = ((bs[pos] & 0b11) as usize) << 8 | bs[pos+1] as usize;
+                let length = ((bs.get(pos)? >> 2) + 3 as u8) as usize;
+                let distance = ((bs.get(pos)? & 0b11) as usize) << 8 | *bs.get(pos+1)? as usize;
                 pos += 2;
                 let pivot = out.len();
                 out.resize(pivot+length, 0);
@@ -35,7 +35,7 @@ pub fn decompress(bs: &[u8], size: usize) -> Vec<u8> {
         }
     }
     out.truncate(size);
-    out
+    Some(out)
 }
 
 #[repr(C)]
@@ -56,14 +56,17 @@ pub extern fn decompress_external(bs_ptr: *const u8, bs_size: usize, size: usize
         bs = slice::from_raw_parts(bs_ptr, bs_size);
     }
     let out = decompress(bs, size);
-    let ret = DecompressionResult{success: true, size: out.len(), data: out.as_ptr(), capacity: out.capacity() };
+    let ret = match out {
+        Some(ref v) => DecompressionResult{success: true, size: v.len(), data: v.as_ptr(), capacity: v.capacity() },
+        None => DecompressionResult{success: false, size: 0, data: bs_ptr, capacity: 0 }
+    };
     mem::forget(out);
     ret
 }
 
 #[no_mangle]
 pub extern fn free_result(result: DecompressionResult) {
-    let s = unsafe {
+    unsafe {
         vec::Vec::from_raw_parts(result.data as (*mut u8), result.size, result.capacity)
     };
 }
@@ -88,7 +91,7 @@ mod tests {
         let mut outfile = fs::File::open(name_out)?;
         let mut decompressed: Vec<u8> = Vec::new();
         outfile.read_to_end(&mut decompressed)?;
-        let data = decompress(compressed.as_slice(), decompressed.len());
+        let data = decompress(compressed.as_slice(), decompressed.len()).unwrap();
         assert_eq!(data, decompressed);
         Ok(())
     }
